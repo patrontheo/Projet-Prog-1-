4663e064b49dc26569bb43ed2f7d65a43f095ed3
Romain Palazzo <romainpalazzo@macbook-pro-de-romain.local>
2018-03-23 00:09:17 +0100

    work15

--- ProjetProg/testOscillateur.cc
+++ ProjetProg/testOscillateur.cc
@@ -9,6 +9,8 @@ void test_oscillateur()
 {
     Vecteur vec1(3.0, 4.0, 5.0);
     Vecteur vec2(1.0, -2.5, 7.25);
+    Vecteur vec3(9.0, 11.0, 5.6);
+    Vecteur vec4(3.2, 4.5 , 6.7);
     Oscillateur test1(vec1, vec2);
     double t (5.0);
     
@@ -18,7 +20,13 @@ void test_oscillateur()
     cout<<"Sa fonction d'évolution donne pour t: "<<t<<test1.equation_evolution(5)<<endl;
     
     cout<<"Test de set() : "<<endl;
-    
+    cout<<"De son parametre : "<<vec3<<endl;
+    cout<<" donne : "<<endl;
+    test1.set_p(vec3);
+    cout<<test1.get_p()<<" #parametre"<<endl;
+    test1.set_v(vec4);
+    cout<<"De sa vitesse : "<<vec4<<endl;
+    cout<<test1.get_v()<<endl;
     
     
     
